5ff0f0d7e8d71487b5e88c0360bd81f284414f08
Pedro <pedrobfontoura@gmail.com>
2021-10-12 12:14:38 -0300

    debug true

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-04ewt9x^(p@5fsjbn983_hg^p%9zt+i*$$p2gr&4vp2+1+ywzn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
